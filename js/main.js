3603dffea7d5e0b09ac82f0a5ba7e8fb0d0ecc1f
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-10-12 19:31:12 -0300

    Adicionando menu para telas menores

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 
 function navBar(){
-	document.getElementById('menu').style.display = 'block';
+	document.getElementById('menu').style.display = 'none';
 	document.getElementById('nav-bar').style.display = 'none';
+	document.getElementById('nav-menu').style.display = 'block';
 }
