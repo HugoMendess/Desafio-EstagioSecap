1f6b50922d0a30b50548f418f3c3205a35395f8a
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-10-13 11:26:40 -0300

    Alterando layout

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 
 
 function navBar(){
-	document.getElementById('menu').style.display = 'none';
+
 	document.getElementById('nav-bar').style.display = 'none';
 	document.getElementById('nav-menu').style.display = 'block';
+}
+
+function navBarClose(){
+	document.getElementById('nav-bar').style.display = 'block';
+	document.getElementById('nav-menu').style.display = 'none';
 }
