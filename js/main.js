30b89ee6410c00cccd9dc8b2cdb466dea551ea14
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-10-14 19:14:30 -0300

    Finalizando desafio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 
-
+//Função para abrir o menu mobile
 function navBar(){
 
 	document.getElementById('nav-bar').style.display = 'none';
 	document.getElementById('nav-menu').style.display = 'block';
 }
 
+//Função para fechar o menu mobile
 function navBarClose(){
 	document.getElementById('nav-bar').style.display = 'block';
 	document.getElementById('nav-menu').style.display = 'none';
